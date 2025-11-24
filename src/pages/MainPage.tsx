203501077b1530207a2d4095c03dbe260ec989ad
mrpe4enushkaa_ <daniillavrika1980@gmail.com>
2025-11-24 20:00:27 +0300

    filter

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -3,12 +3,12 @@ import { SortingName } from "../models/sorting.enum";
 import Card from "../components/Card";
 import Wrapper from "../components/Wrapper";
 import Footer from "../components/Footer";
-import { useEffect, useState } from "react";
+import { useMemo, useState } from "react";
 import SortList from "../components/SortList";
 
 export default function MainPage() {
     const [sorting, setSorting] = useState<string>(SortingName.Nearest);
-    const [sortedEvents, setSortedEvents] = useState<IEvent[]>([]);
+    const [searchQuery, setSearchQuery] = useState<string>("");
 
     const events: IEvent[] = [
         {
@@ -39,33 +39,63 @@ export default function MainPage() {
             registration: true,
             price: 150
         },
+        {
+            name: "Мероприятие 1",
+            date: "02.01.2026",
+            time: "09:00",
+            registration: true,
+            price: 0
+        },
+        {
+            name: "Шампупуни от пупуни",
+            date: "19.01.2026",
+            time: "12:10",
+            registration: true,
+            price: 100
+        },
+        {
+            name: "Мероприятие 2",
+            date: "15.12.2025",
+            time: "20:45",
+            registration: false,
+            price: 50
+        },
+        {
+            name: "Мероприятие 3",
+            date: "01.01.2025",
+            time: "15:40",
+            registration: true,
+            price: 150
+        },
     ];
 
-    useEffect(() => {
-        const sortEvents = () => {
-            const eventsCopy = [...events];
+    // const getEvents = () => { }
 
-            switch (sorting) {
-                case SortingName.Nearest:
-                    return eventsCopy.sort((a, b) => {
-                        const dateA = new Date(a.date.split('.').reverse().join('-'));
-                        const dateB = new Date(b.date.split('.').reverse().join('-'));
-                        return dateA.getTime() - dateB.getTime();
-                    });
+    const sortAndFilterEvents = useMemo(() => {
+        let eventsCopy = [...events];
 
-                case SortingName.Cheaper:
-                    return eventsCopy.sort((a, b) => a.price - b.price);
+        eventsCopy = eventsCopy.filter(event =>
+            event.name.toLocaleLowerCase().includes(searchQuery.toLocaleLowerCase())
+        );
 
-                case SortingName.Expensive:
-                    return eventsCopy.sort((a, b) => b.price - a.price);
+        switch (sorting) {
+            case SortingName.Nearest:
+                return eventsCopy.sort((a, b) => {
+                    const dateA = new Date(a.date.split('.').reverse().join('-'));
+                    const dateB = new Date(b.date.split('.').reverse().join('-'));
+                    return dateA.getTime() - dateB.getTime();
+                });
 
-                default:
-                    return eventsCopy;
-            }
-        };
+            case SortingName.Cheaper:
+                return eventsCopy.sort((a, b) => a.price - b.price);
 
-        setSortedEvents(sortEvents());
-    }, [sorting]);
+            case SortingName.Expensive:
+                return eventsCopy.sort((a, b) => b.price - a.price);
+
+            default:
+                return eventsCopy;
+        }
+    }, [sorting, searchQuery]);
 
     return (
         <main className="main">
@@ -85,13 +115,13 @@ export default function MainPage() {
                         <div className="main-text">
                             <div className="search-block">
                                 <div className="fix-div">
-                                    <svg className="search-icon" xmlns="http://www.w3.org/2000/svg" width="17" height="17"
-                                        viewBox="0 0 17 17" fill="none">
-                                        <path
-                                            d="M16 16L11 11M1 6.83333C1 7.59938 1.15088 8.35792 1.44404 9.06565C1.73719 9.77339 2.16687 10.4164 2.70854 10.9581C3.25022 11.4998 3.89328 11.9295 4.60101 12.2226C5.30875 12.5158 6.06729 12.6667 6.83333 12.6667C7.59938 12.6667 8.35792 12.5158 9.06565 12.2226C9.77339 11.9295 10.4164 11.4998 10.9581 10.9581C11.4998 10.4164 11.9295 9.77339 12.2226 9.06565C12.5158 8.35792 12.6667 7.59938 12.6667 6.83333C12.6667 6.06729 12.5158 5.30875 12.2226 4.60101C11.9295 3.89328 11.4998 3.25022 10.9581 2.70854C10.4164 2.16687 9.77339 1.73719 9.06565 1.44404C8.35792 1.15088 7.59938 1 6.83333 1C6.06729 1 5.30875 1.15088 4.60101 1.44404C3.89328 1.73719 3.25022 2.16687 2.70854 2.70854C2.16687 3.25022 1.73719 3.89328 1.44404 4.60101C1.15088 5.30875 1 6.06729 1 6.83333Z"
-                                            stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                                    <svg className="search-icon" xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 17 17" fill="none">
+                                        <path d="M16 16L11 11M1 6.83333C1 7.59938 1.15088 8.35792 1.44404 9.06565C1.73719 9.77339 2.16687 10.4164 2.70854 10.9581C3.25022 11.4998 3.89328 11.9295 4.60101 12.2226C5.30875 12.5158 6.06729 12.6667 6.83333 12.6667C7.59938 12.6667 8.35792 12.5158 9.06565 12.2226C9.77339 11.9295 10.4164 11.4998 10.9581 10.9581C11.4998 10.4164 11.9295 9.77339 12.2226 9.06565C12.5158 8.35792 12.6667 7.59938 12.6667 6.83333C12.6667 6.06729 12.5158 5.30875 12.2226 4.60101C11.9295 3.89328 11.4998 3.25022 10.9581 2.70854C10.4164 2.16687 9.77339 1.73719 9.06565 1.44404C8.35792 1.15088 7.59938 1 6.83333 1C6.06729 1 5.30875 1.15088 4.60101 1.44404C3.89328 1.73719 3.25022 2.16687 2.70854 2.70854C2.16687 3.25022 1.73719 3.89328 1.44404 4.60101C1.15088 5.30875 1 6.06729 1 6.83333Z" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                                     </svg>
-                                    <input type="text" id="search" className="search unbounded-light" placeholder="Поиск" />
+                                    <input type="text" id="search" className="search unbounded-light" placeholder="Поиск" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
+                                    <svg className="clear-search" style={{ display: searchQuery ? "block" : "none" }} xmlns="http://www.w3.org/2000/svg" width="9" height="9" viewBox="0 0 9 9" fill="none" onClick={() => setSearchQuery("")}>
+                                        <path d="M1.16667 8.33333L4.16667 5.33333L7.16667 8.33333L8.33333 7.16667L5.33333 4.16667L8.33333 1.16667L7.16667 0L4.16667 3L1.16667 0L0 1.16667L3 4.16667L0 7.16667L1.16667 8.33333Z" fill="#1D1B20" />
+                                    </svg>
                                 </div>
                             </div>
                         </div>
@@ -110,7 +140,7 @@ export default function MainPage() {
             <section className="main-events">
                 <Wrapper>
                     <div className="main-events-container">
-                        {sortedEvents.map((event, index) => (
+                        {sortAndFilterEvents.map((event, index) => (
                             <Card event={event} key={index} />
                         ))}
                     </div>
