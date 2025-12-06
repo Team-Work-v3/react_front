452cf27eafa09bd479bcc79878f8778917caac1e
mrpe4enushkaa_ <daniillavrika1980@gmail.com>
2025-12-06 18:24:29 +0300

    base: process.env.npm_lifecycle_event === 'build' ? '/index/' : '/'

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/index/',   // <-- чтобы пути стали index/assets/...
+  base: process.env.npm_lifecycle_event === 'build' ? '/index/' : '/',
   plugins: [react()],
 })
