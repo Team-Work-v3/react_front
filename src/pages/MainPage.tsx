f2ad41c4e656e7e8ce191bdb1f83d6e1f6808faa
mrpe4enushkaa_ <daniillavrika1980@gmail.com>
2026-02-13 18:59:14 +0300

    active

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -243,7 +243,7 @@ export default function MainPage() {
                         <span className="type-of-events-button" ref={typeOfEvents.pastButton} onClick={openTypeEventPast}>Прошедшие</span>
                     </div>
                     <div className="type-of-event-content">
-                        <div className="main-events-container" ref={typeOfEvents.presentContainer}>
+                        <div className="main-events-container active" ref={typeOfEvents.presentContainer}>
                             {sortAndFilterEvents.length !== 0 ?
                                 sortAndFilterEvents.map((event, index) => (
                                     <Card event={event} key={index} />
