c10259657b0f028f9be3b53ecaf29bf208e5dc0e
VVK <vvkuprik@gmail.com>
2026-03-07 14:11:40 +0300

    тест

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -113,7 +113,7 @@ export default function MainPage() {
 
             const dataCategories = await categories.json();
             console.log(dataCategories);
-            setCategories(dataCategories.category);
+            setCategories(dataCategories);
         }
         fetchEvents();
     }, []);
