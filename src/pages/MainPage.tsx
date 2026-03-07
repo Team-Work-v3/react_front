c1278f8deec407e9252b9fe3217090213364d7c5
VVK <vvkuprik@gmail.com>
2026-03-07 11:12:03 +0300

    new api in category

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -109,7 +109,7 @@ export default function MainPage() {
             setEventsBack(dataBack.events);
             setEvents(data.events);
 
-            const categories = await fetch("http://62.109.16.129:5000/api/getCategory");
+            const categories = await fetch("http://62.109.16.129:5000/api/getCategoryNew");
 
             const dataCategories = await categories.json();
             console.log(dataCategories);
