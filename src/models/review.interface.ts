638a49ff722445e485505026b1970ed719294b83
khamster <vara.khamster2.0@gmail.com>
2026-03-27 17:13:45 +0300

    api

--- src/models/review.interface.ts
+++ src/models/review.interface.ts
@@ -1,10 +1,19 @@
 
+// export interface IReview {
+//     id: number;
+//     event_id: number;
+//     author_name: string;
+//     text: string;
+//     rating?: number; // если есть рейтинг
+//     created_at: string;
+//     updated_at?: string;
+// }
+
 export interface IReview {
-    id: number;
-    event_id: number;
-    author_name: string;
-    text: string;
-    rating?: number; // если есть рейтинг
-    created_at: string;
-    updated_at?: string;
+  review_id: number;
+  event_id: number;
+  user_name: string;
+  review_text: string;
+  created_at: string;
+  is_approved: boolean;
 }
