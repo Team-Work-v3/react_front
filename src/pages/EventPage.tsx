477c9cfd68de5626b62d3d06d3ff00a9af377ab3
khamster <vara.khamster2.0@gmail.com>
2026-04-04 11:45:48 +0300

    mb

--- src/pages/EventPage.tsx
+++ src/pages/EventPage.tsx
@@ -1201,7 +1201,7 @@ export default function EventPage() {
                                     {reviewColumns[0].map((review) => (
                                         <div key={review.review_id} className="review-card">
 
-                                            <div class="blok-with-avatar">
+                                            <div>
                                                 <div className="review-avatar">
                                                     {review.image_link ? (
                                                         <img 
