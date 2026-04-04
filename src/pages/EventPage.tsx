1d6cab2685694425ccd7c742b1c8b1732489e360
khamster <vara.khamster2.0@gmail.com>
2026-04-04 11:26:32 +0300

    test

--- src/pages/EventPage.tsx
+++ src/pages/EventPage.tsx
@@ -1196,64 +1196,60 @@ export default function EventPage() {
                             <div className="reviews-empty inter-regular">Пока нет отзывов. Будьте первым!</div>
                         )}
                         {!loading && !error && reviews.length > 0 && (
-    <div className="reviews-list inter-regular">
-        {/* Левая колонка */}
-        <div className="reviews-list-container odd">
-            {reviewColumns[0].map((review) => (
-                <div key={review.review_id} className="review-card">
-                    <div className="review-avatar">
-                        {review.image_link ? (
-                            <img 
-                                src={`http://62.109.16.129:5000${review.image_link}`} 
-                                alt={review.user_name}
-                                className="review-avatar-img"
-                            />
-                        ) : (
-                            <div className="review-avatar-placeholder">
-                                {review.user_name.charAt(0).toUpperCase()}
-                            </div>
-                        )}
-                    </div>
-                    <div className="review-content">
-                        <div className="review-header">
-                            <h3 className="review-author">{review.user_name}</h3>
-                            <span className="review-date">{formatReviewDate(review.created_at)}</span>
-                        </div>
-                        <p className="review-text">{review.review_text}</p>
-                    </div>
-                </div>
-            ))}
-        </div>
+                            <div className="reviews-list inter-regular">
+                                <div className="reviews-list-container odd">
+                                    {reviewColumns[0].map((review) => (
+                                        <div key={review.review_id} className="review-card">
+                                       <div>
+                                        <div>
+                                            <img src={`http://62.109.16.129:5000${review.image_link}`} ></img>
+                                            <div>
+                                                <p>Анатолий Петрович</p>
+                                                <p>13 апреля, 2025 года</p>
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <p>Также как внедрение современных методик обеспечивает широкому кругу (специалистов) участие в формировании позиций, занимаемых участниками в отношении поставленных задач. 
+Являясь всего лишь частью общей картины, тщательные исследования конкурентов набирают популярность среди определенных слоев населения, а значит, должны быть своевременно верифицированы. 
+Являясь всего лишь частью общей картины, тщательные исследования </p>
+                                        </div>
+                                       </div>
 
-        {/* Правая колонка */}
-        <div className="reviews-list-container even">
-            {reviewColumns[1].map((review) => (
-                <div key={review.review_id} className="review-card">
-                    <div className="review-avatar">
+                    {/* <div className="review-avatar">
                         {review.image_link ? (
-                            <img 
-                                src={`http://62.109.16.129:5000${review.image_link}`} 
-                                alt={review.user_name}
-                                className="review-avatar-img"
-                            />
+                        <img 
+                            src={`http://62.109.16.129:5000${review.image_link}`} 
+                            alt={review.user_name}
+                            className="review-avatar-img"
+                        />
                         ) : (
-                            <div className="review-avatar-placeholder">
-                                {review.user_name.charAt(0).toUpperCase()}
-                            </div>
+                        <div className="review-avatar-placeholder">
+                            {review.user_name.charAt(0).toUpperCase()}
+                        </div>
                         )}
                     </div>
                     <div className="review-content">
                         <div className="review-header">
-                            <h3 className="review-author">{review.user_name}</h3>
-                            <span className="review-date">{formatReviewDate(review.created_at)}</span>
+                        <h3 className="review-author">{review.user_name}</h3>
+                        <span className="review-date">{formatDate(review.created_at)}</span>
                         </div>
                         <p className="review-text">{review.review_text}</p>
                     </div>
-                </div>
-            ))}
-        </div>
-    </div>
-)}
+                    </div> */}
+                                    {/* ))} */}
+                                </div>
+                                <div className="reviews-list-container even">
+                                    {reviewColumns[1].map((review) => (
+                                        <div key={review.review_id} className="review-card">
+                                            <h3 className="text-little-medium">{review.user_name}</h3>
+                                            {/* http://62.109.16.129:5000{review.image_link}  */}
+                                            <h6 className="text-little">{formatDate(review.created_at)}</h6>
+                                            <p className="text-little">{review.review_text}</p>
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
+                        )}
                     </div>
                 )}
 
