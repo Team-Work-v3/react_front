c0cf47bac1dd8b961a783beadc4efc01c32a0c19
khamster <vara.khamster2.0@gmail.com>
2026-04-16 15:59:57 +0300

    test6

--- src/App.tsx
+++ src/App.tsx
@@ -11,26 +11,6 @@ export default function App() {
         <Route path="/event/:id" element={<EventPage />} />
       </Routes>
     </BrowserRouter>
-    <Swiper
-  spaceBetween={12}
-  loop={true}
-  navigation={{
-    nextEl: '.custom-swiper-button-right',
-    prevEl: '.custom-swiper-button-left',
-  }}
-  modules={[Navigation]}
-  breakpoints={{
-    0: {
-      slidesPerView: 1.2,
-    },
-    768: {
-      slidesPerView: 2,
-    },
-    1024: {
-      slidesPerView: 4,
-    }
-  }}
-  className="mySwiper"
-></Swiper>
+   
   )
 }
